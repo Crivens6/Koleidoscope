59cd854752dbd4b936cb0e1942826d63bf2cbd01
Christyllin <DiskworldWizzard@Gmail.com>
2025-12-18 13:14:54 -0800

    Complete step 5. Error on tutorial demo extern call of putchard. Present in original code.

--- toy.cpp
+++ toy.cpp
@@ -6,6 +6,7 @@
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
@@ -50,7 +51,10 @@ enum Token
 
     tok_if = -6,
     tok_then = -7,
-    tok_else = -8
+    tok_else = -8,
+
+    tok_for = -9,
+    tok_in = -10
 };
 
 static std::string IdentifierStr; // Filled in if tok_identifier
@@ -92,6 +96,12 @@ static int gettok()
         }
         if(IdentifierStr == "else"){
             return tok_else;
+        }
+        if(IdentifierStr == "for"){
+            return tok_for;
+        }
+        if(IdentifierStr == "in"){
+            return tok_in;
         }
         return tok_identifier;
     }
@@ -194,6 +204,17 @@ class IfExprAST : public ExprAST {
     public:
     IfExprAST(std::unique_ptr<ExprAST> Cond, std::unique_ptr<ExprAST> Then, std::unique_ptr<ExprAST> Else)
     :Cond(std::move(Cond)), Then(std::move(Then)), Else(std::move(Else)){}
+    Value *codegen() override;
+};
+
+/// @brief  ForExprAST - Expression class for for/in
+class ForExprAST : public ExprAST {
+    std:: string VarName;
+    std::unique_ptr<ExprAST> Start, End, Step, Body;
+
+    public:
+    ForExprAST(const std::string &VarName, std::unique_ptr<ExprAST> Start, std::unique_ptr<ExprAST> End, std::unique_ptr<ExprAST> Step, std::unique_ptr<ExprAST> Body)
+    : VarName(VarName), Start(std::move(Start)), End(std::move(End)), Step(std::move(Step)), Body(std::move(Body)){}
     Value *codegen() override;
 };
 
@@ -302,7 +323,7 @@ static std::unique_ptr<ExprAST> ParseIdentifierExpr()
 
             if (CurTok != ',')
             {
-                return LogError("Expected '(' or ',' in arguments list)");
+                return LogError("Expected ')' or ',' in arguments list)");
             }
             getNextToken();
         }
@@ -345,11 +366,64 @@ static std::unique_ptr<ExprAST> ParseIfExpr(){
     return std::make_unique<IfExprAST>(std::move(Cond), std::move(Then),std::move(Else));
 }
 
+// forexpr ::= 'for' identifier '=' expr ',' expr (',' expr)?
+static std::unique_ptr<ExprAST> ParseForExpr(){
+    getNextToken();
+
+    if(CurTok != tok_identifier){
+        return LogError("expected identifier after for");
+    }
+
+    std::string IdName = IdentifierStr;
+    getNextToken();
+
+    if(CurTok != '='){
+        return LogError("expected '=' after for");
+    }
+    getNextToken();
+
+    auto Start = ParseExpression();
+    if(!Start){
+        return nullptr;
+    }
+    if(CurTok != ','){
+        return LogError("expected ',' after for start value");
+    }
+    getNextToken();
+
+    auto End = ParseExpression();
+    if(!End){
+        return nullptr;
+    }
+
+    // The Step value is optional
+    std::unique_ptr<ExprAST> Step;
+    if(CurTok == ','){
+        getNextToken();
+        Step = ParseExpression();
+        if(!Step){
+            return nullptr;
+        }
+    }
+
+    if(CurTok != tok_in){
+        return LogError("expected 'in' after for");
+    }
+    getNextToken();
+
+    auto Body = ParseExpression();
+    if(!Body){
+        return nullptr;
+    }
+    return std::make_unique<ForExprAST>(IdName, std::move(Start), std::move(End), std::move(Step), std::move(Body));
+}
+
 static std::unique_ptr<ExprAST> ParsePrimary()
 {
     switch (CurTok)
     {
     default:
+        fprintf(stderr, "%s\n", IdentifierStr.c_str());
         return LogError("Unknown token when expecting an expression");
     case tok_identifier:
         return ParseIdentifierExpr();
@@ -359,6 +433,8 @@ static std::unique_ptr<ExprAST> ParsePrimary()
         return ParseParenExpr();
     case tok_if:
         return ParseIfExpr();
+    case tok_for:
+        return ParseForExpr();
     }
 }
 
@@ -606,7 +682,7 @@ Value *IfExprAST::codegen(){
     Builder->SetInsertPoint(ThenBB);
 
     Value *ThenV = Then->codegen();
-    if(!Then){
+    if(!ThenV){
         return nullptr;
     }
 
@@ -619,7 +695,7 @@ Value *IfExprAST::codegen(){
     Builder->SetInsertPoint(ElseBB);
 
     Value *ElseV = Else->codegen();
-    if(!Else){
+    if(!ElseV){
         return nullptr;
     }
 
@@ -635,6 +711,88 @@ Value *IfExprAST::codegen(){
     PN->addIncoming(ThenV, ThenBB);
     PN->addIncoming(ElseV, ElseBB);
     return PN;
+}
+
+Value *ForExprAST::codegen(){
+    //Emit the start code first, without 'variable' in scope
+    Value *StartVal = Start->codegen();
+    if(!StartVal){
+        return nullptr;
+    }
+
+
+
+    // Make the new basic block for the loop header, inserting after current block
+    Function *TheFunction = Builder->GetInsertBlock()->getParent();
+    BasicBlock *PreheaderBB = Builder->GetInsertBlock();
+    BasicBlock *LoopBB = BasicBlock::Create(*TheContext, "loop", TheFunction);
+
+    //Insert an explicit fall through from the current block to the LoopBB
+    Builder->CreateBr(LoopBB);
+
+    // Start insertion in LoopBB
+    Builder->SetInsertPoint(LoopBB);
+
+    // Start the PHI node with an entry for Start
+    PHINode *Variable = Builder->CreatePHI(Type::getDoubleTy(*TheContext), 2, VarName);
+    Variable->addIncoming(StartVal, PreheaderBB);
+
+    // Within the loop, the variable is defined equal to the PHI node. If it shadow an existing variable, we have to restore it, so save it now
+    Value *OldVal = NamedValues[VarName];
+    NamedValues[VarName] = Variable;
+
+    // Emit the boy of the loop. This, like any other epr, can change the current BB. Note that we ignore the value computed by the body, but don't allow an error
+    if(!Body->codegen()){
+        return nullptr;
+    }
+
+    // emit the step value
+    Value *StepVal = nullptr;
+    if(Step) {
+        StepVal = Step->codegen();
+        if(!StepVal){
+            return nullptr;
+        
+        }
+    }else {
+        // If not specified, use 1.0
+        StepVal = ConstantFP::get(*TheContext, APFloat(1.0));
+    }
+
+    Value *NextVar = Builder->CreateFAdd(Variable, StepVal, "nextvar");
+
+    // Compute the end condition
+    Value *EndCond = End->codegen();
+    if(!EndCond){
+        return nullptr;
+    }
+
+    // Convert condition to a bool by comparing non-equal to 0.0
+    EndCond = Builder->CreateFCmpONE(EndCond, ConstantFP::get(*TheContext, APFloat(0.0)), "loopcond");
+
+    // Create the "after loop" block and insert it
+    BasicBlock *LoopEndBB = Builder->GetInsertBlock();
+    BasicBlock *AfterBB = BasicBlock::Create(*TheContext, "afterloop:", TheFunction);
+
+    // Insert the conditional branch into the end of LoopEndBB
+    Builder->CreateCondBr(EndCond, LoopBB, AfterBB);
+
+    // Any new code will be inserted in After BB
+    Builder->SetInsertPoint(AfterBB);
+
+    // Add a new entry to the PHI node for the backedge
+    Variable->addIncoming(NextVar, LoopEndBB);
+
+    // Restore the unshadowed variable
+    if(OldVal){
+        NamedValues[VarName] = OldVal;
+    }else{
+        NamedValues.erase(VarName);
+    }
+
+    // for expr always returns 0.0
+    return Constant::getNullValue(Type::getDoubleTy(*TheContext));
+
 }
 
 Value *CallExprAST::codegen()
@@ -726,7 +884,7 @@ static void InitializeModuleAndManagers(void)
     Builder = std::make_unique<IRBuilder<>>(*TheContext);
 
     // Create new pass and analysis managers
-    TheFPM = std::make_unique<FunctionPassManager>(); ////////////// ERROR
+    TheFPM = std::make_unique<FunctionPassManager>(); ////////////// ERROR, Changed
     TheLAM = std::make_unique<LoopAnalysisManager>();
     TheFAM = std::make_unique<FunctionAnalysisManager>();
     TheCGAM = std::make_unique<CGSCCAnalysisManager>();
@@ -855,6 +1013,23 @@ static void DriverLoop()
     }
 }
 
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+/// putchard - putchar that takes a double and returns 0.
+extern "C" DLLEXPORT double putchard(double X) {
+  fputc((char)X, stderr);
+  return 0;
+}
+
+/// printd - printf that takes a double prints it as "%f\n", returning 0.
+extern "C" DLLEXPORT double printd(double X) {
+  fprintf(stderr, "%f\n", X);
+  return 0;
+}
 
 int main()
 {
