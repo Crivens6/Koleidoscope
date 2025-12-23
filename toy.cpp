2f163a0ba6638cb6bdfdb3fc8ebe2e381f737891
Christyllin <DiskworldWizzard@Gmail.com>
2025-12-22 16:10:30 -0800

    Complete through step 6

--- toy.cpp
+++ toy.cpp
@@ -54,7 +54,10 @@ enum Token
     tok_else = -8,
 
     tok_for = -9,
-    tok_in = -10
+    tok_in = -10,
+
+    tok_binary = -11,
+    tok_unary = -12
 };
 
 static std::string IdentifierStr; // Filled in if tok_identifier
@@ -102,6 +105,12 @@ static int gettok()
         }
         if(IdentifierStr == "in"){
             return tok_in;
+        }
+        if(IdentifierStr == "binary"){
+            return tok_binary;
+        }
+        if(IdentifierStr == "unary"){
+            return tok_unary;
         }
         return tok_identifier;
     }
@@ -186,6 +195,18 @@ public:
     Value *codegen() override;
 };
 
+/// UnaryExprAST - Expression class for a unary operator
+class UnaryExprAST : public ExprAST {
+    char Opcode;
+    std::unique_ptr<ExprAST> Operand;
+
+public:
+    UnaryExprAST(char Opcode, std::unique_ptr<ExprAST> Operand)
+    : Opcode(Opcode), Operand(std::move(Operand)) {}
+
+  Value *codegen() override;
+};
+
 // CallEsprAst - Expression node for function calls
 class CallExprAST : public ExprAST
 {
@@ -224,10 +245,25 @@ class PrototypeAST
     std::string Name;
     std::vector<std::string> Args;
 
+    bool IsOperator;
+    unsigned Precedence;
+
 public:
-    PrototypeAST(const std::string &Name, std::vector<std::string> Args) : Name(Name), Args(std::move(Args)) {}
+    PrototypeAST(const std::string &Name, std::vector<std::string> Args, bool IsOperator = false, unsigned Prec = 0) 
+    : Name(Name), Args(std::move(Args)), IsOperator(IsOperator), Precedence(Prec) {}
+
     Function *codegen();
-    const std::string &getName() const { return Name; }
+    const std::string &getName() const {return Name;}
+
+    bool isUnaryOp() const {return IsOperator && Args.size() == 1;}
+    bool isBinaryOp() const {return IsOperator && Args.size() == 2;}
+
+    char getOperatorName() const {
+        assert(isUnaryOp() || isBinaryOp());
+        return Name[Name.size() -1];
+    }
+
+    unsigned getBinaryPrecedence() const {return Precedence;}
 };
 
 // FunctionAST - Node represents a function definition
@@ -494,6 +530,21 @@ static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec, std::unique_ptr<Expr
     }
 }
 
+static std::unique_ptr<ExprAST> ParseUnary(){
+    // If the current token is not an operator, it must be a primery expr
+    if(!isascii(CurTok) || CurTok ==  '(' || CurTok == ','){
+        return ParsePrimary();
+    }
+
+    // If this is a unary operator, read it
+    int Opc = CurTok;
+    getNextToken();
+    if(auto Operand = ParseUnary()){
+        return std::make_unique<UnaryExprAST>(Opc, std::move(Operand));
+    }
+    return nullptr;
+}
+
 static std::unique_ptr<ExprAST> ParseExpression()
 {
     auto LHS = ParsePrimary();
@@ -507,13 +558,51 @@ static std::unique_ptr<ExprAST> ParseExpression()
 
 static std::unique_ptr<PrototypeAST> ParsePrototype()
 {
-    if (CurTok != tok_identifier)
+    std::string FnName;
+    unsigned Kind = 0; // 0 = identifier, 1 = unary, 2 = binary
+    unsigned BinaryPrecedence = 30;
+
+
+    switch (CurTok)
     {
+    default:
         return LogErrorP("Expected function name in prototype");
+    case tok_identifier:
+        FnName = IdentifierStr;
+        Kind = 0;
+        getNextToken();
+        break;
+    case tok_unary:
+        getNextToken();
+        if(!isascii(CurTok)){
+            return LogErrorP("Expected unary operator");
+        }
+        FnName = "unary";
+        FnName += (char)CurTok;
+        Kind = 1;
+        getNextToken();
+        break;
+    case  tok_binary:
+        getNextToken();
+        if(!isascii(CurTok)){
+            return LogErrorP("Expected binary operator");
+        }
+        FnName = "binary";
+        FnName += char(CurTok);
+        Kind = 2;
+        getNextToken();
+
+        // Read the precedence if present
+        if(CurTok == tok_number){
+            if(NumVal < 1 || NumVal > 100){
+                return LogErrorP("Invalid precedence: must be 1..100");
+            }
+            BinaryPrecedence = (unsigned)NumVal;
+            getNextToken();
+        }
+        break;
     }
-    // Get the name of the function
-    std::string FnName = IdentifierStr;
-    getNextToken();
+    
     if (CurTok != '(')
     {
         return LogErrorP("Expected '(' in prototype");
@@ -530,8 +619,13 @@ static std::unique_ptr<PrototypeAST> ParsePrototype()
         return LogErrorP("Expected ')' in prototype");
     }
     getNextToken();
+    //Verify right number of names for operator
+    if(Kind && ArgNames.size() != Kind){
+        return LogErrorP("Invalid number of operands for operator");
+    }
+
     // Return a node containing a function name and its argument names
-    return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+    return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames), Kind != 0, BinaryPrecedence);
 }
 
 static std::unique_ptr<FunctionAST> ParseDefinition()
@@ -657,8 +751,29 @@ Value *BinaryExprAST::codegen()
         L = Builder->CreateFCmpUGT(L, R, "cmpgtmp");
         return Builder->CreateUIToFP(L, Type::getDoubleTy(*TheContext), "booltmp");
     default:
-        return LogErrorV("invalid binary operator");
+        break;
     }
+
+    // If it wasn't a builtin binary operator, it must be a user defiend on. Emit a call to it
+    Function *F = getFunction(std::string("binary") + Op);
+    assert(F && "binary operator not found!");
+
+    Value *Ops[2] = {L,R};
+    return Builder->CreateCall(F,Ops, "binop");
+
+}
+
+Value *UnaryExprAST::codegen(){
+    Value *OperandV = Operand->codegen();
+    if(!OperandV){
+        return nullptr;
+    }
+
+    Function *F = getFunction(std::string("unary")+ Opcode);
+    if(!F){
+        return LogErrorV("Unknown unary operator");
+    }
+    return Builder->CreateCall(F,OperandV, "unop");
 }
 
 Value *IfExprAST::codegen(){
@@ -838,39 +953,44 @@ Function *PrototypeAST::codegen()
 }
 
 Function *FunctionAST::codegen() {
-  // Transfer ownership of the prototype to the FunctionProtos map, but keep a
-  // reference to it for use below.
-  auto &P = *Proto;
-  FunctionProtos[Proto->getName()] = std::move(Proto);
-  Function *TheFunction = getFunction(P.getName());
-  if (!TheFunction)
+    // Transfer ownership of the prototype to the FunctionProtos map, but keep a
+    // reference to it for use below.
+    auto &P = *Proto;
+    FunctionProtos[Proto->getName()] = std::move(Proto);
+    Function *TheFunction = getFunction(P.getName());
+    if (!TheFunction)
+        return nullptr;
+        
+    // If this is an operator, install it
+    if(P.isBinaryOp()){
+        BinopPrecedence[P.getOperatorName()] = P.getBinaryPrecedence();
+    }
+
+    // Create a new basic block to start insertion into.
+    BasicBlock *BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
+    Builder->SetInsertPoint(BB);
+
+    // Record the function arguments in the NamedValues map.
+    NamedValues.clear();
+    for (auto &Arg : TheFunction->args())
+        NamedValues[std::string(Arg.getName())] = &Arg;
+
+    if (Value *RetVal = Body->codegen()) {
+        // Finish off the function.
+        Builder->CreateRet(RetVal);
+
+        // Validate the generated code, checking for consistency.
+        verifyFunction(*TheFunction);
+
+        // Run the optimizer on the function.
+        TheFPM->run(*TheFunction, *TheFAM);
+
+        return TheFunction;
+    }
+
+    // Error reading body, remove function.
+    TheFunction->eraseFromParent();
     return nullptr;
-
-  // Create a new basic block to start insertion into.
-  BasicBlock *BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
-  Builder->SetInsertPoint(BB);
-
-  // Record the function arguments in the NamedValues map.
-  NamedValues.clear();
-  for (auto &Arg : TheFunction->args())
-    NamedValues[std::string(Arg.getName())] = &Arg;
-
-  if (Value *RetVal = Body->codegen()) {
-    // Finish off the function.
-    Builder->CreateRet(RetVal);
-
-    // Validate the generated code, checking for consistency.
-    verifyFunction(*TheFunction);
-
-    // Run the optimizer on the function.
-    TheFPM->run(*TheFunction, *TheFAM);
-
-    return TheFunction;
-  }
-
-  // Error reading body, remove function.
-  TheFunction->eraseFromParent();
-  return nullptr;
 }
 
 /////////////////////////////////////////////////////////////// Top Level Parser and JIT Driver
